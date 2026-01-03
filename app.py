98976d0b83cee2864e1e444c30795ec5b7c484ec
jhnnce <leydimark1510@gmail.com>
2026-01-02 21:32:13 -0500

    cambio de api

--- app.py
+++ app.py
@@ -2,29 +2,32 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse, HTMLResponse
 import requests
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
 
-def get_sunrise(lat, lng, dia='today', tz="America/Lima"):
-    url = "https://api.sunrise-sunset.org/json"
-    params = {"lat": lat, "lng": lng, "date": dia, "formatted": 0, 'tzid': tz}
+def get_sunrise(lat, lng, dia='today', timezone="America/Lima"):
+    url = "https://api.sunrisesunset.io/json"
+    params = {"lat": lat, "lng": lng, "date": dia, "timezone": timezone}
     data = requests.get(url, params=params).json()["results"]
+
+    sunrise_utc = data["sunrise"]
+    sunset_utc = data["sunset"]
+    date = data["date"]
+
     
-    sunrise_utc = datetime.fromisoformat(data["sunrise"])
-    sunset_utc = datetime.fromisoformat(data["sunset"])
-    local_tz = pytz.timezone(tz)
-    sunrise_local = sunrise_utc.astimezone(local_tz)
-    sunset_local = sunset_utc.astimezone(local_tz)
-    print(sunrise_local)
-    return sunrise_local, sunset_local
+    return date, sunrise_utc, sunset_utc
 
 
-
-def get_current_value(sunrise_time, tz="America/Lima"):
+def get_current_value(date, sunrise_time, tz="America/Lima"):
     now = datetime.now(pytz.timezone(tz))
-    minutes_passed = int((now - sunrise_time).total_seconds() / 60)
+    sunrise_time = datetime.strptime(date + " " + sunrise_time, "%Y-%m-%d %I:%M:%S %p")
+
+    now = datetime.now(pytz.timezone("America/Lima"))
+    minutos = now.time().strftime("%H:%M:%S").split(":")
+    combined = datetime.combine(datetime.today(), datetime.strptime(f"{minutos[0]}:{minutos[1]}:{minutos[2]}", "%H:%M:%S").time())
+    minutes_passed = int((combined - sunrise_time).total_seconds() / 60)
 
     if minutes_passed < 0:
         return None  # aún no amanece
@@ -36,24 +39,81 @@ def get_current_value(sunrise_time, tz="America/Lima"):
 LAT = -13.53195
 LNG = -71.96746
 
+def next_tatwa_time(hora_inicial: str, repeticiones: int = 60) -> None:
+    """Calcula y muestra las siguientes horas en el ciclo de TATWA."""
+    formato = "%I:%M:%S %p %Y-%m-%d"
+
+    # Convertir texto a objeto datetime
+    t = datetime.strptime(hora_inicial, formato)
+    # Intervalo de 24 minutos
+    intervalo = timedelta(minutes=24)
+
+    w = 1
+    valor=''
+    for _ in range(repeticiones):
+
+ 
+            t += intervalo
+            w += 1
+            
+            if w > 5:
+                w = 1
+            
+            if w == 1:
+                
+                value_name = "AKASH"
+
+            elif w == 2:
+        
+                value_name = "VAYÚ"
+
+            elif w == 3:
+        
+                value_name = "TEJAS"
+
+        
+            elif w == 4:
+            
+                value_name = "PRITHVI"
+
+            elif w == 5:
+            
+                value_name = "APAS"
+
+            
+            if t > datetime.now():
+                if value_name== "APAS":
+                    valor += f'{t.time().strftime("%I:%M:%S %p")} | {value_name}<br><br>'
+                else:
+                    valor += f'{t.time().strftime("%I:%M:%S %p")} | {value_name}<br>'
+
+    return valor
 
 def get_sunrise_value():
 
-    sunrise, sunset = get_sunrise(LAT, LNG)
+    date, sunrise, sunset = get_sunrise(LAT, LNG)
+    sunrise = datetime.strptime(sunrise, "%I:%M:%S %p")
+    sunset = datetime.strptime(sunset, "%I:%M:%S %p")
+    date = datetime.strptime(date, "%Y-%m-%d")
+ 
+
     now = datetime.now(pytz.timezone("America/Lima"))
-    if now < sunrise:
+    if now.date() < date.date():
         dia='yesterday'
     else:
         dia='today'
-    sunrise, sunset = get_sunrise(LAT, LNG, dia)
-    amanecer = sunrise.strftime("%H:%M (%Y-%m-%d)")
-    atardecer = sunset.strftime("%H:%M (%Y-%m-%d)")
-    print(sunrise, sunset)
-    now = datetime.now(pytz.timezone("America/Lima"))
-    actual = now.strftime("%H:%M")
-    if get_current_value(sunrise):
+    
 
-        value, now, minutes = get_current_value(sunrise)
+    date, sunrise, sunset = get_sunrise(LAT, LNG, dia)
+    amanecer = sunrise
+    atardecer = sunset
+    actual = now.strftime("%H:%M")
+
+    #next_tatwa_time(sunrise+' '+ date)
+
+    if get_current_value(date, sunrise):
+
+        value, now, minutes = get_current_value(date, sunrise)
         enTatwa = minutes % 24
         restante = 24 - minutes % 24
         valor = value
@@ -62,11 +122,11 @@ def get_sunrise_value():
 
         if int(value) == 1:
                 
-            value_name = "AKASHA"
+            value_name = "AKASH"
 
         elif int(value) == 2:
         
-            value_name = "VAYU"
+            value_name = "VAYÚ"
 
         elif int(value) == 3:
     
@@ -74,7 +134,7 @@ def get_sunrise_value():
         
         elif int(value) == 4:
         
-            value_name = "PRITHIVI"
+            value_name = "PRITHVI"
 
         elif int(value) == 5:
         
@@ -84,10 +144,8 @@ def get_sunrise_value():
 
 
 
-        return {"amanecer": amanecer, 'atardecer': atardecer, "actual": actual, 'now': now.date().isoformat(),  'minutos':minutes,"enTatwa": enTatwa, 'restante': restante, 'valor': valor, 'nombre_valor': value_name}
-    else:
-        situacion='AUN NO AMANECE'
-        return {"amanecer": amanecer, 'atardecer': atardecer, "actual": actual, 'now': now.date().isoformat(),  'minutos':situacion, "enTatwa": situacion, 'restante': situacion, 'valor': situacion, 'nombre_valor': situacion}
+        return {"amanecer": amanecer, 'atardecer': atardecer, 'date_sun':date,"actual": actual, 'now': now.date().isoformat(),  'minutos':minutes,"enTatwa": enTatwa, 'restante': restante, 'valor': valor, 'nombre_valor': value_name, 'proximos': next_tatwa_time(sunrise+' '+ date) }
+    
 
 
 @app.get("/value")
@@ -107,13 +165,14 @@ def home():
                     let data = await r.json();
                     document.getElementById("val").innerHTML = 'Fecha Actual: ' + data.now + '<br>' +
                         'Hora Actual: ' + data.actual + '<br>' +
-                        'Salida de Sol: ' + data.amanecer + '<br>' +
-                        'Puesta de Sol: ' + data.atardecer + '<br>' +
+                        'Salida de Sol: ' + data.amanecer + ' (' + data.date_sun + ')' + '<br>' +
+                        'Puesta de Sol: ' + data.atardecer + ' (' + data.date_sun + ')' + '<br>' +
                         'Minutos desde el Amanecer: ' + data.minutos + '<br>' + '<br>' +
                         'TATWA: ' + data.nombre_valor + '<br>' +
                         'Minutos pasados en el Tatwa: ' + data.enTatwa + '<br>' +
                         'Minutos restantes en el Tatwa: ' + data.restante + '<br>' +
-                        'Valor: ' + data.valor + '<br>';
+                        'Valor: ' + data.valor + '<br>' + '<br>' +
+                        'Próximos Tatwas:<br>'+'<br>' + data.proximos + '<br>';
                         
                 }
                 setInterval(load, 20000);
